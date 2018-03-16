66309010cbf33626b2bed89519671aff9c7ad4e2
Agustin Fiori <agustin.fiori@m8agency.com>
2018-03-16 18:33:56 -0300

    Views, el regreso
    Vista con bootstrap

--- productos/views.py
+++ productos/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Categoria
+
+def index(request):
+    latest_question_list = Categoria.objects.order_by('name')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'productos/abc.html', context)
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hola!! Este es el index de PRODUCTOS")
+#def index(request):
+#    return HttpResponse("Hola!! Este es el index de PRODUCTOS")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse("You're looking at question %s." % question_id) 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
